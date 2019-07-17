1c9050c5cc7fddbe26b55a3ca0d5e8255fb80326
pratiti <trivediricha5@gmail.com>
2019-07-17 14:03:13 +0530

    script file

---
+++ script.py
@@ -0,0 +1,7 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 16:07:46) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x = 1
+if x == 1:
+    # indented four spaces
+    print("x is 1.")
+    print("Goodbye, World!")
